af097b6ec9a29a2fe5a032892786cef0065b7bc6
Sairam Raja <sairamraja1201@gmail.com>
2026-08-28 21:01:28 +0530

    Add Student class with display method

---
+++ action or operation.java
@@ -0,0 +1,11 @@
+class Student {
+
+    void display() {
+        System.out.println("Hello Student");
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.display();
+    }
+}
